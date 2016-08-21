00a9ac0e4c11095305b587aa95b8ad361a268ee2
Brekk <brekk@brekkbockrath.com>
2016-08-21 10:09:07 -0700

    Adding more / better Task behavior (#4)

--- cli.js
+++ cli.js
@@ -1,21 +1,32 @@
 #!/usr/bin/env node
-const {file} = require(`./lib/util/jsx-to-pug`).default
+const {file: jsxFile} = require(`./lib/util/jsx-to-pug`).default
 const {json} = require(`./lib/util/fs`).default
-const yaml = require('./lib/util/yaml').default
+const {yaml, yamlFile} = require(`./lib/util/yaml`)
+const {writeFile} = require(`./lib/util/task`)
 const program = require(`commander`)
+const path = require(`path`)
+
+const readJSONOrYAML = (file) => {
+  const extension = path.extname(file)
+  if (extension === `.yml` || extension === `.yaml`) {
+    return yamlFile(file)
+  }
+  return json(file)
+}
 
 program
   .version(`0.0.1`)
   .option(`-d, --debug`, `log additional info`)
   .option(`-p, --props <p>`, `raw props`, yaml)
-  .option(`-f, --file <f>`, `prop file`, json)
+  .option(`-f, --file <f>`, `prop file`, readJSONOrYAML)
+  .option(`-o, --output <o>`, `save output to file`)
   .parse(process.argv)
 
 if (!program.props && !program.file) {
   process.stderr.write([
-    `glass needs props in order to render`,
-    `(glass -p "text: value\nlink: item" file.jsx) or`,
-    `(glass -f file.json file.jsx)\n`
+    `glass needs props in order to render\n`,
+    `(glass -p "text: value\\nlink: item" file.jsx) or`,
+    `(glass -f <file.json|yml> file.jsx)\n`
   ].join(` `))
   process.exit(1)
 }
@@ -28,7 +39,24 @@ if (!filename) {
 
 const props = program.props || program.file
 
-file.toPug(props, filename).fork(
-  (e) => process.stderr.write(e),
-  (p) => process.stdout.write(p)
-)
+// const writeToFile = writeFile(program.output)
+
+const writers = {
+  // error: program.output ? writeToFile : process.stderr.write,
+  // success: program.output ? writeToFile : process.stdout.write
+  error: (e) => process.stderr.write(e),
+  success: (o) => process.stdout.write(o)
+}
+
+try {
+  const outcome = jsxFile.toPug(props, filename)
+  outcome.fork(
+    writers.error,
+    writers.success
+  )
+} catch (e) {
+  console.log(`error while running glass cli - `, e.toString())
+  if (e.stack) {
+    console.log(e.stack)
+  }
+}
--- src/util/task.js
+++ src/util/task.js
@@ -1,5 +1,31 @@
 import Task from 'data.task'
-// import curry from 'lodash/fp/curry'
+import reduce from 'lodash/fp/reduce'
+import filter from 'lodash/fp/filter'
+import curry from 'lodash/fp/curry'
+import fs from 'fs'
+import _debug from 'debug'
+const debug = _debug(`glass:util:task`)
+
+const assertTasks = filter((x) => {
+  return !!x.fork
+})
+
+export const sequence = (listOfTasks) => {
+  if (listOfTasks.length === 0 || assertTasks(listOfTasks).length !== listOfTasks.length) {
+    throw new TypeError(`Expected to be given a list of tasks with fork methods.`)
+  }
+  const [initial, ...remaining] = listOfTasks
+  return reduce((lastTask, newTask) => {
+    return lastTask.chain((a) => {
+      return newTask.map((b) => {
+        if (!Array.isArray(a)) {
+          a = [a]
+        }
+        return a.concat(b)
+      })
+    }, newTask)
+  }, initial, remaining)
+}
 
 export const promiseToTask = (promise) => {
   return new Task((reject, resolve) => {
@@ -35,9 +61,33 @@ export const rejectTask = (x) => {
   })
 }
 
+export const writeData = curry((reject, resolve, filename, raw) => {
+  debug(`attempting to write to ${filename}: ${raw}`)
+  fs.writeFile(filename, raw, `utf8`, (err) => {
+    debug(err || `outcome: success`)
+    if (err) {
+      return reject(err)
+    }
+    resolve(true)
+  })
+})
+
+export const writeFile = curry(function _writeFile(filename, data) {
+  return new Task(function _writeFileTaskRR(reject, resolve) {
+    if (data && data.fork) {
+      data.fork(reject, writeData(reject, resolve, filename))
+      return
+    }
+    writeData(reject, resolve, filename, data)
+  })
+})
+
 export default {
   reject: rejectTask,
   resolve: resolveTask,
   fromCB: cbToTask,
-  fromPromise: promiseToTask
+  fromPromise: promiseToTask,
+  writeFile,
+  writeData,
+  sequence
 }
