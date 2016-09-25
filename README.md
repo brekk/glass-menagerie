d91bff362a563f80a76d8d7fc07a1415aa81900b
brekk <brekk@brekkbockrath.com>
2016-09-25 16:03:24 -0700

    Lots of stuff working, but probably the specific flags need to be reworked before merging.

--- README.md
+++ README.md
@@ -1,12 +1,86 @@
 ![](https://circleci.com/gh/brekk/glass-menagerie.svg?style=shield&circle-token=55f2885e65a163dee6ea47f7aa1c71c81b0d552c)
 
 # glass-menagerie
-> "Yes, I have tricks in my pocket, I have things up my sleeve. But I am the opposite of a stage magician. He gives you illusion that has the appearance of truth. I give you truth in the pleasant disguise of illusion."
+## Toolkit to convert jsx to pug (formerly jade)
 > "How beautiful it is, and how easily it can be broken."
 > - Tennessee Williams
 
-### Toolkit to convert jsx to pug (formerly jade)
+### Description
 
-This library may be potentially brittle for other people's use cases; but the goal is to help facilitate snapshots of an existing codebase (written in JSX) as `.pug` / `.jade` output, which is then consumed further downstream in a codepen / jsfiddle environment.
+This module is used to a sample file like [this](https://github.com/brekk/glass-menagerie/blob/master/tests/fixtures/fixture-blog-post-jsx.js):
 
-Broadly, jade / pug is easier to manipulate and has a lower learning curve for non-developers; this tool will help designers on the team have a living style-guide which is easily modifiable. (That's the goal anyway.)
+```js
+const React = require(`react`)
+const {PropTypes: types} = React
+
+export const Blog = (props) => (
+  <div className="blog-post">
+    <h1>{props.title}</h1>
+    <strong>{props.number}</strong>
+    <p>{props.description}</p>
+  </div>
+)
+
+export const propTypes = {
+  title: types.string,
+  number: types.number,
+  description: types.string
+}
+
+Blog.propTypes = propTypes
+
+export default Blog
+```
+
+Into a simple `pug` (formerly `jade`) representation with values [auto-generated](https://github.com/brekk/glass-menagerie/blob/master/src/proptypes.js#L150-L172) based on the exported `propTypes` value:
+
+```pug
+//- tests/fixtures/fixture-blog-post-jsx.js
+.blog-post
+  h1 ljvzxdbkor
+  strong 673
+  p jrkbwvezxl
+```
+
+### Goal
+
+* To create a pipeline in which simple React (for now, with eventual plans for other various analogous frameworks) components can be represented as `pug` "snapshots".
+  - It is likely that this snapshots will become brittle (and will require re-generation) over time, but ideally these can be used to represent lightweight versions of existing components as a proxy for rapid prototyping and iteration using something like [Codepen](https://codepen.io)
+
+### Installation
+```sh
+npm i glass -S
+```
+
+### CLI Usage
+
+```sh
+glass --help
+```
+Output:
+```sh
+Usage: glass [options]
+
+Options:
+
+  -h, --help               output usage information
+  -V, --version            output the version number
+  -a, --auto-mock          automagically pull exported 'propTypes' in
+  -p, --props <p>          raw props
+  -q, --prop-file <f>      prop file
+  -o, --output <o>         save output to file (defaults to stdout)
+  -w, --no-write           don't write output to files, just output to stdout
+  -n, --allow-empty-props  be lax on the explicit propTypes export rule
+```
+
+To auto-mock and convert your files, add this to your `package.json`:
+
+```json
+"glass": {
+  "files": [
+    "tests/fixtures/fixture-*jsx.js"
+  ],
+  "output": "codepen",
+  "allowEmptyProps": false
+}
+```
--- src/cli/glass.js
+++ src/cli/glass.js
@@ -13,7 +13,9 @@ import globby from 'globby'
 import zip from 'lodash/fp/zip'
 import curry from 'lodash/fp/curry'
 import map from 'lodash/fp/map'
+import id from 'lodash/fp/identity'
 import flow from 'lodash/fp/flow'
+import {makeTracer} from 'f-utility/dev/debug'
 // import forEach from 'lodash/fp/forEach'
 import {mergePairs} from 'f-utility/fp/merge-pairs'
 import {yaml} from '../yaml'
@@ -21,9 +23,23 @@ register()
 const noop = () => {}
 noop(poly)
 
+const GLASS = `glass-menagerie`
+
+const _trace = makeTracer(GLASS)
+const trace = {
+  generate: _trace([`cli`, `generate`])
+}
+
 const debug = _debug(`glass-menagerie:cli`)
 
-import {readJSONOrYAML, glassMenagerie, autoMock, makePugKeyValues} from '../glass-menagerie'
+import {requireWithPropTypes} from '../proptypes'
+
+import {
+  readJSONOrYAML,
+  glassMenagerie,
+  autoMock,
+  makePugKeyValues
+} from '../glass-menagerie'
 
 const relativePath = curry(function _relativePath(dir, x) {
   return path.relative(dir, x)
@@ -36,6 +52,38 @@ const writeFiles = (out) => {
   return out
 }
 
+async function generate(config, relativize, allowError, noWrite) {
+  debug(`config`, config)
+  const relFiles = map(relativize, config.files)
+  debug(`relFiles`, relFiles)
+  const files = await globby(relFiles)
+  debug(`files`, files)
+  const mockAll = map(autoMock(allowError))
+  const mockups = mockAll(files)
+  const writeOrPipe = noWrite ? id : writeFiles
+  debug(`allow write?`, noWrite)
+  return Promise.all(mockups).then((converted) => {
+    debug(`conversions`, converted)
+    try {
+      const list = flow(
+        trace.generate(`# input`),
+        zip(files),
+        trace.generate(`# zipped`),
+        map(makePugKeyValues(config, relativize)),
+        trace.generate(`# keyed`),
+        map(writeOrPipe),
+        trace.generate(`# write or piped`),
+        mergePairs,
+        trace.generate(`# output`)
+      )(converted)
+      process.stdout.write(JSON.stringify(list, null, 2))
+    } catch (e) {
+      process.stderr.write(e)
+      process.exit(1)
+    }
+  }).catch((e) => process.stderr.write(e.toString()))
+}
+
 async function run() {
   const json = await readPkgUp()
   const dir = await pkgDir()
@@ -43,80 +91,84 @@ async function run() {
 
   const {pkg} = json
   const {glass: config} = pkg
+
+  function list(val) {
+    return val.split(`,`)
+  }
+
   program
     .version(pkg.version)
+    .option(`-a, --auto-mock`, `automagically pull exported 'propTypes' in`)
     .option(`-p, --props <p>`, `raw props`, yaml)
-    .option(`-f, --file <f>`, `prop file`, readJSONOrYAML)
-    .option(`-o, --output <o>`, `save output to file`)
-    .option(`-a, --allow-empty-props`, `be lax on the explicit propTypes export rule`)
+    .option(`-q, --prop-file <q>`, `prop file`, readJSONOrYAML)
+    .option(`-f, --files, --file <f>`, list)
+    .option(`-o, --output <o>`, `save output to file (defaults to stdout)`)
+    .option(`-w, --no-write`, `don't write output to files, just output to stdout`)
+    .option(`-n, --allow-empty-props`, `be lax on the explicit propTypes export rule`)
     .parse(process.argv)
 
-  if (!program.props && !program.file) {
-    if (!config || !config.files || (!config.output && !program.output)) {
+  const rawProps = program.props || program.propFile
+  const output = program.output || config.output
+  const noWrite = program.noWrite || config.noWrite || false
+  if (output && !noWrite) {
+    await mkdirp(relativize(output))
+  }
+  const allowError = program.allowEmptyProps || config.allowEmptyProps
+  debug(`__ settings:`, {...program, ...config})
+  if (!rawProps && !program.autoMock) {
+    if (!config || (!config.output && !program.output)) {
       process.stderr.write([
         `glass needs flags in order to render\n`,
         `(glass -p "text: value\\nlink: item" file.jsx) or`,
-        `(glass -f <file.json|yml> file.jsx)\n`,
+        `(glass -q <file.json|yml> file.jsx)\n`,
         `or set a "glass" config in your package.json\n`
       ].join(` `))
       process.exit(1)
     }
-    const output = program.output || config.output
-    const allowError = program.allowEmptyProps || config.allowEmptyProps
-    await mkdirp(relativize(output))
-
-    debug(`config`, config)
-    const relFiles = map(relativize, config.files)
-    debug(`relFiles`, relFiles)
-    const files = await globby(relFiles)
-    const mockAll = map(autoMock(allowError))
-    const mockups = mockAll(files)
-    debug(`mockups`, mockups)
-    Promise.all(mockups).then((converted) => {
-      debug(`conversions`, converted)
-      try {
-        const list = flow(
-          zip(files),
-          map(makePugKeyValues(config, relativize)),
-          map(writeFiles),
-          mergePairs
-        )(converted)
-        process.stdout.write(JSON.stringify(list, null, 2))
-      } catch (e) {
-        process.stderr.write(e)
-        process.exit(1)
-      }
-    }).catch((e) => process.stderr.write(e.toString()))
+    generate(config, relativize, allowError, noWrite)
     return
   }
-
-  const filename = program.args[0]
+  const refFiles = program.files.length > 1 ?
+    program.files :
+    program.files[0]
+  const filename = program.args[0] || refFiles
   if (!filename) {
-    process.stderr.write(`glass expected to be given filename to consume (glass -f json file.js)\n`)
+    process.stderr.write(
+      `glass expected to be given filename to consume (glass -q props.json file.js)\n`
+    )
     process.exit(1)
   }
 
-  const props = program.props || program.file
-
-  // const writeToFile = writeFile(program.output)
-
   const writers = {
-    // error: program.output ? writeToFile : process.stderr.write,
-    // success: program.output ? writeToFile : process.stdout.write
     error: (e) => process.stderr.write(e),
     success: (o) => process.stdout.write(o)
   }
-
   try {
+    let props = {}
+    debug(`filename`, filename)
+    const filenameIsGlobby = Array.isArray(filename) || filename.indexOf(`*`) > -1
+    if (filenameIsGlobby) {
+      const genConfig = {...config, files: [filename]}
+      genConfig.allowEmptyProps = allowError
+      genConfig.output = output
+      generate(genConfig, relativize, allowError, noWrite)
+      return
+    }
+    if (!rawProps && allowError && program.autoMock) {
+      props = await requireWithPropTypes(allowError, filename)
+    } else {
+      writers.error(`Expected to be given props or to find them within the files.`)
+      return
+    }
     const outcome = glassMenagerie(props, filename)
     outcome.fork(
       writers.error,
       writers.success
     )
   } catch (e) {
-    process.stderr.write(`error while running glass cli - ` + e.toString())
+    writers.error(`error while running glass cli - ` + e.toString())
     if (e.stack) {
-      process.stderr.write(e.stack)
+      writers.error(e.stack)
     }
   }
 }
